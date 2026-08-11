48be6c5bf75173502b402b183112ca3c30e76acc
mylilcat <824447374@qq.com>
2026-08-11 10:49:05 +0800

    更改启动方式

--- launcher/launcher.go
+++ launcher/launcher.go
@@ -2,10 +2,9 @@ package launcher
 
 import (
 	"github.com/gorilla/websocket"
+	"github.com/mylilcat/savior"
 	"github.com/mylilcat/savior/log"
 	"github.com/mylilcat/savior/net"
-	"github.com/mylilcat/savior/util"
-	"time"
 )
 
 const (
@@ -16,42 +15,42 @@ const (
 
 var (
 	//server port 服务端口
-	port string
-
-	//server prototype 服务器协议类型
-	proto string
-
-	//connection idle detection 连接空闲检测
-	IMonitor *net.IdleMonitor
-
-	webSocketMessageType int
+	//port string
+	//
+	////server prototype 服务器协议类型
+	//proto string
+	//
+	////connection idle detection 连接空闲检测
+	//IMonitor *net.IdleMonitor
+	//
+	//webSocketMessageType int
 
 	enableDebugLog bool
 )
 
 // SetProto set server proto. 设置服务协议
-func SetProto(p string) {
-	proto = p
-}
-
-// SetPort set server port. 设置服务端口
-func SetPort(serverPort string) {
-	port = serverPort
-}
-
-// SetIdleMonitor set idle timeout,and time unit. time unit,supports down to milliseconds.
-// time.Microsecond, time.Millisecond, time.Second, time.Minute, time.Hour
-// 设置连接空闲检测，读超时时间，写超时时间，超时时间单位。时间单位最小支持到毫秒。
-func SetIdleMonitor(readIdle int64, writeIdle int64, unit time.Duration) {
-	if readIdle < 0 || writeIdle < 0 || !util.IsTimeUnitValid(unit) {
-		return
-	}
-	IMonitor = &net.IdleMonitor{
-		ReadIdle:  readIdle,
-		WriteIdle: writeIdle,
-		Unit:      unit,
-	}
-}
+//func SetProto(p string) {
+//	proto = p
+//}
+//
+//// SetPort set server port. 设置服务端口
+//func SetPort(serverPort string) {
+//	port = serverPort
+//}
+//
+//// SetIdleMonitor set idle timeout,and time unit. time unit,supports down to milliseconds.
+//// time.Microsecond, time.Millisecond, time.Second, time.Minute, time.Hour
+//// 设置连接空闲检测，读超时时间，写超时时间，超时时间单位。时间单位最小支持到毫秒。
+//func SetIdleMonitor(readIdle int64, writeIdle int64, unit time.Duration) {
+//	if readIdle < 0 || writeIdle < 0 || !util.IsTimeUnitValid(unit) {
+//		return
+//	}
+//	IMonitor = &net.IdleMonitor{
+//		ReadIdle:  readIdle,
+//		WriteIdle: writeIdle,
+//		Unit:      unit,
+//	}
+//}
 
 func EnableDebugLog() {
 	enableDebugLog = true
@@ -63,52 +62,44 @@ func DebugLogInit() {
 	}
 }
 
-func SetWebSocketMessageType(t int) {
-	webSocketMessageType = t
-}
+//func SetWebSocketMessageType(t int) {
+//	webSocketMessageType = t
+//}
 
 // ServerStart server start. 服务启动
-func ServerStart() {
+func ServerStart(s *savior.Savior) {
 	if enableDebugLog {
 		log.NewSaviorLogger()
 	}
-	switch proto {
+	switch s.Proto {
 	case TCP:
-		s := new(net.TCPServer)
-		s.Port = port
-		port = ""
-		s.Handler = net.NewHandler()
-		s.IdleMonitor = IMonitor
-		IMonitor = nil
+		server := new(net.TCPServer)
+		server.Port = s.Port
+		server.Handler = s.Handler
+		server.IdleMonitor = s.IdleMonitor
 		s.Start()
 	case KCP:
-		s := new(net.KCPServer)
-		s.Port = port
-		port = ""
-		s.Handler = net.NewHandler()
-		s.IdleMonitor = IMonitor
-		IMonitor = nil
+		server := new(net.KCPServer)
+		server.Port = s.Port
+		server.Handler = s.Handler
+		server.IdleMonitor = s.IdleMonitor
 		s.Start()
 	case WS:
-		s := new(net.WSServer)
-		s.Port = port
-		port = ""
-		s.Handler = net.NewHandler()
-		s.IdleMonitor = IMonitor
-		IMonitor = nil
-		if webSocketMessageType > 0 {
-			s.MessageType = webSocketMessageType
+		server := new(net.WSServer)
+		server.Port = s.Port
+		server.Handler = s.Handler
+		server.IdleMonitor = s.IdleMonitor
+		if s.WebSocketMessageType > 0 {
+			server.MessageType = s.WebSocketMessageType
 		} else {
-			s.MessageType = websocket.TextMessage
+			server.MessageType = websocket.TextMessage
 		}
 		s.Start()
 	default:
-		s := new(net.TCPServer)
-		s.Port = port
-		port = ""
-		s.Handler = net.NewHandler()
-		s.IdleMonitor = IMonitor
-		IMonitor = nil
+		server := new(net.TCPServer)
+		server.Port = s.Port
+		server.Handler = s.Handler
+		server.IdleMonitor = s.IdleMonitor
 		s.Start()
 	}
 }
--- savior.go
+++ savior.go
@@ -1,60 +1,163 @@
 package savior
 
 import (
-	"github.com/mylilcat/savior/launcher"
+	"github.com/gorilla/websocket"
+	"github.com/mylilcat/savior/log"
 	"github.com/mylilcat/savior/net"
 	"github.com/mylilcat/savior/service"
+	"github.com/mylilcat/savior/util"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
 
-func Start(services ...*service.Service) {
-	launcher.DebugLogInit()
-	for _, s := range services {
-		service.Register(s)
+const (
+	TCP = "tcp"
+	KCP = "kcp"
+	WS  = "ws"
+)
+
+var enableDebugLog bool
+
+type Savior struct {
+	Port                 string
+	Proto                string
+	IdleMonitor          *net.IdleMonitor
+	Handler              *net.Handler
+	WebSocketMessageType int
+}
+
+func New() *Savior {
+	s := new(Savior)
+	s.Handler = &net.Handler{}
+	return s
+}
+
+func (s *Savior) Start(services ...*service.Service) {
+	//launcher.DebugLogInit()
+	if enableDebugLog {
+		log.NewSaviorLogger()
+	}
+	for _, srv := range services {
+		service.Register(srv)
 	}
 	service.ServicesRun()
-	launcher.ServerStart()
+	//launcher.ServerStart(s)
+	s.ServerStart()
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	service.ServicesStop()
 }
 
-func BindPort(port string) {
-	launcher.SetPort(port)
+func (s *Savior) BindPort(port string) {
+	s.Port = port
 }
 
-func SetProto(p string) {
-	launcher.SetProto(p)
+func (s *Savior) SetProto(proto string) {
+	s.Proto = proto
 }
 
-func SetOnConnectHandler(f func(c net.Connection)) {
-	net.OnConnect = f
+func (s *Savior) SetWebSocketMessageType(webSocketMessageType int) {
+	s.WebSocketMessageType = webSocketMessageType
 }
 
-func SetOnDisconnectHandler(f func(c net.Connection)) {
-	net.OnDisconnect = f
+func (s *Savior) SetIdleMonitor(readIdle int64, writeIdle int64, unit time.Duration) {
+	if readIdle < 0 || writeIdle < 0 || !util.IsTimeUnitValid(unit) {
+		return
+	}
+	s.IdleMonitor = &net.IdleMonitor{
+		ReadIdle:  readIdle,
+		WriteIdle: writeIdle,
+		Unit:      unit,
+	}
 }
 
-func SetOnReadHandler(f func(c net.Connection, data []byte)) {
-	net.OnRead = f
+func (s *Savior) SetOnConnectHandler(f func(c net.Connection)) {
+	s.Handler.OnConnect = f
 }
 
-func SetOnIdleHandler(f func(c net.Connection)) {
-	net.OnIdle = f
+func (s *Savior) SetOnDisconnectHandler(f func(c net.Connection)) {
+	s.Handler.OnDisconnect = f
 }
 
-func SetIdleMonitor(readIdle int64, writeIdle int64, unit time.Duration) {
-	launcher.SetIdleMonitor(readIdle, writeIdle, unit)
+func (s *Savior) SetOnReadHandler(f func(c net.Connection, data []byte)) {
+	s.Handler.OnRead = f
 }
 
-func SetWebSocketMessageType(t int) {
-	launcher.SetWebSocketMessageType(t)
+func (s *Savior) SetOnIdleHandler(f func(c net.Connection)) {
+	s.Handler.OnIdle = f
 }
+func (s *Savior) ServerStart() {
+	if enableDebugLog {
+		log.NewSaviorLogger()
+	}
+	switch s.Proto {
+	case TCP:
+		server := new(net.TCPServer)
+		server.Port = s.Port
+		server.Handler = s.Handler
+		server.IdleMonitor = s.IdleMonitor
+		s.Start()
+	case KCP:
+		server := new(net.KCPServer)
+		server.Port = s.Port
+		server.Handler = s.Handler
+		server.IdleMonitor = s.IdleMonitor
+		s.Start()
+	case WS:
+		server := new(net.WSServer)
+		server.Port = s.Port
+		server.Handler = s.Handler
+		server.IdleMonitor = s.IdleMonitor
+		if s.WebSocketMessageType > 0 {
+			server.MessageType = s.WebSocketMessageType
+		} else {
+			server.MessageType = websocket.TextMessage
+		}
+		s.Start()
+	default:
+		server := new(net.TCPServer)
+		server.Port = s.Port
+		server.Handler = s.Handler
+		server.IdleMonitor = s.IdleMonitor
+		s.Start()
+	}
+}
+
+//func BindPort(port string) {
+//	launcher.SetPort(port)
+//}
+//
+//func SetProto(p string) {
+//	launcher.SetProto(p)
+//}
+//
+//func SetOnConnectHandler(f func(c net.Connection)) {
+//	net.OnConnect = f
+//}
+//
+//func SetOnDisconnectHandler(f func(c net.Connection)) {
+//	net.OnDisconnect = f
+//}
+//
+//func SetOnReadHandler(f func(c net.Connection, data []byte)) {
+//	net.OnRead = f
+//}
+//
+//func SetOnIdleHandler(f func(c net.Connection)) {
+//	net.OnIdle = f
+//}
+//
+//func SetIdleMonitor(readIdle int64, writeIdle int64, unit time.Duration) {
+//	launcher.SetIdleMonitor(readIdle, writeIdle, unit)
+//}
+//
+//func SetWebSocketMessageType(t int) {
+//	launcher.SetWebSocketMessageType(t)
+//}
 
 func EnableDebugLog() {
-	launcher.EnableDebugLog()
+	enableDebugLog = true
 }
